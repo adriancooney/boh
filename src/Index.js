c929e1bb45af8ede529ff2c20d77c96bbfb3da58
Adrian Cooney <cooney.adrian@gmail.com>
2015-04-26 20:36:07 +0100

    Moving Indexer.addFileToIndex to Index class.

--- src/Index.js
+++ src/Index.js
@@ -1,5 +1,8 @@
-var micromatch = require("micromatch"),
-	path = require("path");
+var path = require("path"),
+	boh = require("./boh"),
+	micromatch = require("micromatch"),
+	async = require("async"),
+	debug = require("debug")("boh:index");
 
 /**
  * An index class to describe the file structure.
@@ -25,8 +28,63 @@ Index.prototype.addDirectory = function(path) {
  * Add a file to the index.
  * @param {String} path Path to the file.
  */
-Index.prototype.addFile = function(path) {
-	this.files.push(path);
+Index.prototype.addFile = function(entry, callback) {
+	var self = this;
+
+	// Push the file
+	this.files.push(entry);
+
+	// Get the rules from the file
+	async.waterfall([
+		//Extract the rules from the head of the file
+		boh.extractRulesFromFile.bind(null, entry),
+
+		function(rules, callback) {
+			if(rules.length) {
+				debug("%s rules found.", rules.map(function(rules) {
+					return ("\"" + rules.rule + "\"").blue;
+				}).join(", ").replace(/,\s*([^,]+)$/, " and $1"));
+
+				// Check to see if any of the rules are run at
+				// the indexing phase. If any are applied, remove
+				// them.
+				async.filter(rules, function(rule, filterCallback) {
+					var plugin = boh.getPlugin("indexing", rule.name)
+
+					if(plugin && plugin.phase === "indexing") {
+
+						// Format the rule content
+						rule.content = boh.format(rule.content, {
+							"this": rule.file
+						});
+
+						debug("Running".bold + " %s:%s.", self.relative(rule.file).yellow, rule.name.cyan);
+
+						plugin.execute(rule, self, function(err, built) {
+							// Remove any listeners
+							plugin.removeAllListeners();
+							
+							// I think it's fucking ridiculous async doesn't allow
+							// passing errors in it's filter function. It's to keep
+							// it in line with fs.exists API callback style of truthy
+							// value as the first parameter. First of all, fs.exists
+							// "will be deprecated." (IT'S WRITTEN IN THE DAMN DOCS)
+							// Secondly, ONE function in the ENTIRE NODE.JS API uses
+							// this callback style so you decide to throw out the
+							// callback style of THE REST OF THE ENTIRE ASYNC API?
+							if(err) callback(err);
+							else filterCallback(false);
+						});
+					} else filterCallback(true);
+				}, function(rules) {
+					// Add entry to the index
+					self.addRules(entry, rules);
+
+					callback(null, entry, rules);
+				});
+			} else callback();
+		}
+	], callback);
 };
 
 /**
--- src/Indexer.js
+++ src/Indexer.js
@@ -65,8 +65,14 @@ Indexer.prototype.index = function(directory, options, callback) {
 								// We have a file
 								debug(entry.yellow);
 								
+
 								// Add it to the index
-								self.addFileToIndex(index, entry, callback);
+								index.addFile(entry, function(err, entry, rules) {
+									if(err) return callback(err);
+
+									self.emit("file", entry, rules);
+									callback(null, entry, rules);
+								});
 							}
 						}
 					], callback);
@@ -83,70 +89,6 @@ Indexer.prototype.index = function(directory, options, callback) {
 
 		callback(null, index);
 	});
-};
-
-Indexer.prototype.addFileToIndex = function(index, entry, callback) {
-	var self = this;
-
-	// Add it to the index
-	index.addFile(entry);
-
-	// Get the rules from the file
-	async.waterfall([
-		//Extract the rules from the head of the file
-		boh.extractRulesFromFile.bind(null, entry),
-
-		function(rules, callback) {
-			if(rules.length) {
-				debug("%s rules found.", rules.map(function(rules) {
-					return ("\"" + rules.rule + "\"").blue;
-				}).join(", ").replace(/,\s*([^,]+)$/, " and $1"));
-
-				// Check to see if any of the rules are run at
-				// the indexing phase. If any are applied, remove
-				// them.
-				async.filter(rules, function(rule, filterCallback) {
-					var plugin = boh.getPlugin("indexing", rule.name)
-
-					if(plugin && plugin.phase === "indexing") {
-						// Emit the plugin and rule
-						self.emit("rule", plugin, rule);
-
-						// Format the rule content
-						rule.content = boh.format(rule.content, {
-							"this": rule.file
-						});
-
-						debug("Running".bold + " %s:%s.", index.relative(rule.file).yellow, rule.name.cyan);
-
-						plugin.execute(rule, index, function(err, built) {
-
-							// Remove any listeners
-							plugin.removeAllListeners();
-							
-							// I think it's fucking ridiculous async doesn't allow
-							// passing errors in it's filter function. It's to keep
-							// it in line with fs.exists API callback style of truthy
-							// value as the first parameter. First of all, fs.exists
-							// "will be deprecated." (IT'S WRITTEN IN THE DAMN DOCS)
-							// Secondly, ONE function in the ENTIRE NODE.JS API uses
-							// this callback style so you decide to throw out the
-							// callback style of THE REST OF THE ENTIRE ASYNC API?
-							if(err) callback(err);
-							else filterCallback(false);
-						});
-					} else filterCallback(true);
-				}, function(rules) {
-					self.emit("file", entry, rules);
-
-					// Add entry to the index
-					index.addRules(entry, rules);
-
-					callback();
-				});
-			} else callback();
-		}
-	], callback);
 };
 
 module.exports = Indexer;
